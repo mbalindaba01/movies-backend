1ddf41c400a4413bbbfd8358859ccc7848ed5523
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2022-06-29 11:51:23 +0200

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const route = require('./routes/movieRoutes')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/movies', route)
 
